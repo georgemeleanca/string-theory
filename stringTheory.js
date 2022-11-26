197b76ea22b1a698306a9e8ee1132edd205c8413
George <georgemeleancaa@gmail.com>
2022-11-26 16:01:58 +0200

    Create repo

---
+++ stringTheory.js
@@ -0,0 +1,51 @@
+/**
+ *  ```javascript
+ *  isPalindrome('Mr. Owl ate my metal worm')
+ *  // true
+ *  isPalindrome('Eva, can I see bees in a cave?')
+ *  // true
+ *  ```
+ */
+function isPalindrome(text) {
+
+}
+
+/**
+ *  ```javascript
+ *  isIsogram('uncopyrightables')
+ *  true
+ *  ```
+ */
+function isIsogram(text) {
+
+}
+
+/**
+ *  ```javascript
+ *  isPangram('The quick brown fox jumps over the lazy dog')
+ *  true
+ *  ```
+ */
+function isPangram(text) {
+
+}
+
+/**
+ *  ```javascript
+ *  isAnagram('Justin Timberlake', "I'm a jerk but listen")
+ *  true
+ *  ```
+ */
+function isAnagram(text1, text2) {
+
+}
+
+/**
+ *  ```javascript
+ *  isBlanagram('Justin Timberlake', "I'm a berk but listen")
+ *  true
+ *  ```
+ */
+function isBlanagram(text1, text2) {
+
+}
